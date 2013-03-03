f9657d1490352c0be88ce0e46708ee2ba9010088
Jozef Selesi <selesi@gmail.com>
2013-03-03 13:08:35 +0100

    Extract database configuration into separate recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,34 +7,4 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'mysql::server'
-include_recipe 'database::mysql'
-
-service 'mysql' do
-  action :enable
-end
-
-database_connection = {
-  :host => node[:gallery3][:database][:host],
-  :username => 'root',
-  :password => node[:mysql][:server_root_password]
-}
-
-mysql_database node[:gallery3][:database][:name] do
-  connection database_connection
-  action :create
-end
-
-mysql_database_user node[:gallery3][:database][:username] do
-  connection database_connection
-  password node[:gallery3][:database][:password]
-  action :create
-end
-
-mysql_database_user node[:gallery3][:database][:username] do
-  connection database_connection
-  password node[:gallery3][:database][:password]
-  database_name node[:gallery3][:database][:name]
-  privileges [:all]
-  action :grant
-end
+include_recipe 'gallery::database'
