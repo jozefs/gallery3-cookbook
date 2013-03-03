417985193937d6f34222d728eb532b30fd3f4997
Jozef Selesi <selesi@gmail.com>
2013-03-03 17:12:35 +0100

    Extract installation into separate recipe.

---
+++ recipes/install.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: gallery
+# Recipe:: default
+#
+# Copyright (C) 2013 Gallery3
+#
+# All rights reserved - Do Not Redistribute
+#
+
+database_connection = {
+  :host => node[:gallery3][:database][:host],
+  :username => node[:gallery3][:database][:username],
+  :password => node[:gallery3][:database][:password]
+}
+
+directory "#{node[:gallery3][:webserver][:docroot]}/var" do
+  owner 'vagrant'
+  group 'vagrant'
+  mode 0777
+  action :create
+end
+
+execute 'install Gallery3' do
+  command <<-EOH
+  php index.php \
+    -u #{node[:gallery3][:database][:username]} \
+    -p #{node[:gallery3][:database][:password]} \
+    -d #{node[:gallery3][:database][:name]}
+  EOH
+  cwd "#{node[:gallery3][:webserver][:docroot]}/installer"
+  not_if {File.exists?(node[:gallery3][:webserver][:docroot]+'/var/database.php')}
+  notifies :query, "mysql_database[set default admin info]", :immediately
+end
+
+# set default admin password and email address after installation
+mysql_database 'set default admin info' do
+  connection database_connection
+  database_name node[:gallery3][:database][:name]
+  sql <<-SQL
+  UPDATE users
+  SET
+    password = MD5('#{node[:gallery3][:admin][:password]}'),
+    email = '#{node[:gallery3][:admin][:email]}'
+  WHERE name = 'admin';
+  SQL
+  action :nothing
+end
