6023119d9bbc369808561f19ab87607d941111b5
Jozef Selesi <selesi@gmail.com>
2013-03-03 12:01:52 +0100

    Fill out cookbook maintainer information and description.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             "gallery"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Jozef Selesi"
+maintainer_email "selesi@gmail.com"
 license          "All rights reserved"
-description      "Installs/Configures gallery"
+description      "Installs/Configures Gallery3 development environment"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
