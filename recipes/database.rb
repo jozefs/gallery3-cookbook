90cf74e2775f050e84198327f5419ffff699e73d
Jozef Selesi <selesi@gmail.com>
2013-03-03 16:18:56 +0100

    Create database for unit testing.

--- recipes/database.rb
+++ recipes/database.rb
@@ -11,6 +11,7 @@ database_connection = {
   :password => node[:mysql][:server_root_password]
 }
 
+# create default database
 mysql_database node[:gallery3][:database][:name] do
   connection database_connection
   action :create
@@ -26,6 +27,21 @@ mysql_database_user node[:gallery3][:database][:username] do
   connection database_connection
   password node[:gallery3][:database][:password]
   database_name node[:gallery3][:database][:name]
+  privileges [:all]
+  action :grant
+end
+
+# create unit test database
+unit_test_database_name = "#{node[:gallery3][:database][:name]}_test"
+mysql_database unit_test_database_name do
+  connection database_connection
+  action :create
+end
+
+mysql_database_user node[:gallery3][:database][:username] do
+  connection database_connection
+  password node[:gallery3][:database][:password]
+  database_name unit_test_database_name
   privileges [:all]
   action :grant
 end
