f520ba65f88f9ce96df4e7d4e2267343dd703873
Jozef Selesi <selesi@gmail.com>
2013-03-07 12:38:06 +0100

    Set license to GPL v2, the one Gallery users.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "gallery3"
 maintainer       "Jozef Selesi"
 maintainer_email "selesi@gmail.com"
-license          "All rights reserved"
+license          "GPL v2"
 description      "Installs/Configures Gallery3 development environment"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
